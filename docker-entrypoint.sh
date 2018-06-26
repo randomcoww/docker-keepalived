938f79440af673346a9e2da67982ea822c65be2c
randomcoww <etherblimp@gmail.com>
2018-06-26 00:42:28 -0700

    make config env optional

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-echo -en "$CONFIG" > /etc/keepalived/keepalived.conf
+
+if [ -n "$KEEPALIVED_LOCAL_CONFIG" ]; then
+  echo -en "$KEEPALIVED_LOCAL_CONFIG" > /etc/keepalived/keepalived.conf
+fi
 
 ## start
 rm -f /var/run/keepalived.pid
